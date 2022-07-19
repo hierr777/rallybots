cb8c3fa52edc6249b452f1edb16a8672cee11fd7
hierr777 <hi.illia.lavryniuk@gmail.com>
2022-07-19 22:55:53 +0300

    aaa

--- js/rallybot.js
+++ js/rallybot.js
@@ -1,5 +1,24 @@
 class RallyBot {
-  constructor(name,powerbank) {
-
+  constructor(name,powerbank,motor,frame,moduleSlot,armLeft,armRight,legLeft,legRight,head) {
+    this.name=name;
+    this.powerbank=powerbank;
+    this.motor=motor;
+    this.frame=frame;
+    this.moduleSlot=moduleSlot;
+    this.armLeft=armLeft;
+    this.armRight=armRight;
+    this.legLeft=armLeft;
+    this.legRight=armRight;
+    this.head=head;
+  }
+}
+class PowerBank {
+  constructor(maxCharge) {
+    this.maxCharge=maxCharge;
+    this.charge=maxCharge;
+  }
+  charge(item){
+    item.charge+=item.needingCharge<=this.charge ? item.needingCharge : item.needingCharge-this.charge;
+    this.charge-=item.needingCharge<=this.charge ? item.needingCharge : item.needingCharge-this.charge;
   }
 }
