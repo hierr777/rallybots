962cbad9f45d2ecca1922f81be8392ec97feba2a
hierr777 <hi.illia.lavryniuk@gmail.com>
2022-07-20 19:53:56 +0300

    V1.02

--- js/rallybot.js
+++ js/rallybot.js
@@ -1,8 +1,11 @@
 class RallyBot {
+  #maxCurrentSpeed=0;
+  #coords=Vector(0,0);
   constructor(name,energy,maxSpeed,frame,moduleSlot,armLeftModule,armRightModule,legLeftModule,legRightModule,backModule) {
     this.name=name;
     this.energy=energy;
     this.maxSpeed=maxSpeed;
+    this.#maxCurrentSpeed=maxSpeed;
     this.frame=frame;
     this.moduleSlot=moduleSlot;
     this.armLeftModule=armLeftModule;
@@ -10,5 +13,45 @@ class RallyBot {
     this.legLeftModule=legLeftModule;
     this.legRightModule=legRightModule;
     this.backModule=backModule;
+    this.frame.connectModules(this.armLeftModule,this.armRightModule,this.legLeftModule,this.legRightModule,this.backModule);
+  }
+  constructIn(el,coords){
+    this.#coords=coords;
+    el.innerHTML+=`
+    <div id="${this.name}" class="rallybot" style="left:${this.#coords.x};bottom:${this.#coords.y};">
+      ${this.frame.html}
+    </div>
+    `
+  }
+}
+class Frame {
+  #modules={lhm:undefined,rhm:undefined,llm:undefined,rlm:undefined,bm:undefined}
+  #html=(modules)=>`${modules.lhm.html}${modules.rhm.html}${modules.llm.html}${modules.rlm.html}${modules.bm.html}`;
+  constructor(name,html) {
+    this.name=name;
+    this.#html=html;
+  }
+  connectModules(lhm,rhm,llm,rlm,bm){
+    this.#modules.lhm=lhm;
+    this.#modules.rhm=rhm;
+    this.#modules.llm=llm;
+    this.#modules.rlm=rlm;
+    this.#modules.bm=bm;
+  }
+  get html(){
+    return this.#html(this.#modules);
+  }
+}
+class ModuleSlot {
+  #chanells=[];
+  #module;
+  constructor(chanells,strength) {
+    this.#chanells=chanells;
+    this.strength=strength;
+  }
+  connectModule(module){
+    if (this.strength>module.weight) {
+      this.#module=module;
+    }
   }
 }
