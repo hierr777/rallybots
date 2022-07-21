45189013f1fac3a2c1e24492a4552f22b8b0a198
hierr777 <hi.illia.lavryniuk@gmail.com>
2022-07-21 23:18:19 +0300

    V1.045.5

--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
 var testRallyBot=new RallyBot('prototype',1000,100,baseFrame,new ModuleSlot([{name:"main",inCount:1}],10,10));
 testRallyBot.constructIn(body,Vector(100,100));
+testRallyBot.move(Vector(1,10),cp=>rectangleCollision2D(0,0,Vector(10000,10000),cp,0,Vector(0.5,0.5)));
--- js/rallybot.js
+++ js/rallybot.js
@@ -1,3 +1,99 @@
+//The RallyBot Class
+class RallyBot {
+  #maxCurrentSpeed=0;
+  #coords=Vector(0,0);
+  constructor(name,energy,maxSpeed,frame,moduleSlot) {
+    this.name=name;
+    this.energy=energy;
+    this.maxSpeed=maxSpeed;
+    this.#maxCurrentSpeed=maxSpeed;
+    this.frame=frame;
+  }
+  constructIn(el,coords){
+    this.#coords=coords;
+    el.innerHTML+=`
+    <div id="${this.name}" class="rallybotPoser" style="left:${this.#coords.x}px;bottom:${this.#coords.y}px;">
+      ${this.frame.html}
+    </div>
+    `
+  }
+  connectModules(armLeftModule,armRightModule,legLeftModule,legRightModule,backModule){
+    this.armLeftModule=armLeftModule;
+    this.armRightModule=armRightModule;
+    this.legLeftModule=legLeftModule;
+    this.legRightModule=legRightModule;
+    this.backModule=backModule;
+    this.frame.connectModules(this.armLeftModule,this.armRightModule,this.legLeftModule,this.legRightModule,this.backModule);
+  }
+  move(direction, map)
+  {
+    this.#coords.sum(direction);
+    if(map(this.#coords))
+    {
+      this.#coords.sum(direction.negative);
+    }
+  }
+}
+class Frame {
+  #modules={lhm:undefined,rhm:undefined,llm:undefined,rlm:undefined,bm:undefined}
+  #html=(modules)=>`${modules.lhm.html}${modules.rhm.html}${modules.llm.html}${modules.rlm.html}${modules.bm.html}`;
+  constructor(name,html) {
+    this.name=name;
+    this.#html=html;
+  }
+  connectModules(lhm,rhm,llm,rlm,bm){
+    this.#modules.lhm=lhm;
+    this.#modules.rhm=rhm;
+    this.#modules.llm=llm;
+    this.#modules.rlm=rlm;
+    this.#modules.bm=bm;
+  }
+  get html(){
+    return this.#html(this.#modules);
+  }
+}
+class ModuleSlot {
+  //This is a 1 Dimensional Array, every channel is object in format:{name,inCount}
+  #channels=[];
+  #module;
+  constructor(channels,strength,maxTransportableEnergy) {
+    this.#channels=channels;
+    this.strength=strength;
+    this.maxTransportableEnergy=maxTransportableEnergy;
+  }
+  connectModule(module){
+    //If Strength is 5 and weight is 10 then this won't work
+    //if needing energy larger than can be gived this won`t work
+    if (this.strength>=module.weight&&this.maxTransportableEnergy>=module.needingEnergy) {
+      this.#module=module;
+    }else {
+      alert('wtf u doing, DONT DO THIS!!!!! THIS DONT WORK!!!!!!!!!!');
+    }
+  }
+  useModule(callName,...args){
+    let currChannel=this.#channels.findIndex(c=>c.name===callName);
+    if (this.#channels[currChannel].inCount>=args.length) {
+      this.#module.do(currChannel,...args);
+    }else {
+      alert("too many args");
+    }
+  }
+}
+class Module
+{
+  constructor(name, needingEnergy, works, weight,html)
+  {
+    this.name = name;
+    this.needingEnergy = needingEnergy;
+    this.works = works;
+    this.weight = weight;
+    this.html=html;
+  }
+  do(idx, ...args)
+  {
+    this.works[idx](...args);
+  }
+}
 var baseFrameHtml=(modules)=>`
 <div class="rallybot">
   <div class="scene">
@@ -986,104 +1082,4 @@ var baseFrameHtml=(modules)=>`
     </div>
   </div>
 </div>*/
-
-//The RallyBot Class
-class RallyBot {
-  #maxCurrentSpeed=0;
-  #coords=Vector(0,0);
-  constructor(name,energy,maxSpeed,frame,moduleSlot) {
-    this.name=name;
-    this.energy=energy;
-    this.maxSpeed=maxSpeed;
-    this.#maxCurrentSpeed=maxSpeed;
-    this.frame=frame;
-  }
-  constructIn(el,coords){
-    this.#coords=coords;
-    el.innerHTML+=`
-    <div id="${this.name}" class="rallybotPoser" style="left:${this.#coords.x}px;bottom:${this.#coords.y}px;">
-      ${this.frame.html}
-    </div>
-    `
-  }
-  connectModules(armLeftModule,armRightModule,legLeftModule,legRightModule,backModule){
-    this.armLeftModule=armLeftModule;
-    this.armRightModule=armRightModule;
-    this.legLeftModule=legLeftModule;
-    this.legRightModule=legRightModule;
-    this.backModule=backModule;
-    this.frame.connectModules(this.armLeftModule,this.armRightModule,this.legLeftModule,this.legRightModule,this.backModule);
-  }
-  move(direction = Vector(x,y,z), map())
-  {
-    if(map())
-    {
-      this.#chords -= direction;
-    }
-    else
-    {
-      this.#chords += direction;
-    }
-  }
-}
-class Frame {
-  #modules={lhm:undefined,rhm:undefined,llm:undefined,rlm:undefined,bm:undefined}
-  #html=(modules)=>`${modules.lhm.html}${modules.rhm.html}${modules.llm.html}${modules.rlm.html}${modules.bm.html}`;
-  constructor(name,html) {
-    this.name=name;
-    this.#html=html;
-  }
-  connectModules(lhm,rhm,llm,rlm,bm){
-    this.#modules.lhm=lhm;
-    this.#modules.rhm=rhm;
-    this.#modules.llm=llm;
-    this.#modules.rlm=rlm;
-    this.#modules.bm=bm;
-  }
-  get html(){
-    return this.#html(this.#modules);
-  }
-}
-class ModuleSlot {
-  //This is a 1 Dimensional Array, every channel is object in format:{name,inCount}
-  #channels=[];
-  #module;
-  constructor(channels,strength,maxTransportableEnergy) {
-    this.#channels=channels;
-    this.strength=strength;
-    this.maxTransportableEnergy=maxTransportableEnergy;
-  }
-  connectModule(module){
-    //If Strength is 5 and weight is 10 then this won't work
-    //if needing energy larger than can be gived this won`t work
-    if (this.strength>=module.weight&&this.maxTransportableEnergy>=module.needingEnergy) {
-      this.#module=module;
-    }else {
-      alert('wtf u doing, DONT DO THIS!!!!! THIS DONT WORK!!!!!!!!!!');
-    }
-  }
-  useModule(callName,...args){
-    let currChannel=this.#channels.findIndex(c=>c.name===callName);
-    if (this.#channels[currChannel].inCount>=args.length) {
-      this.#module.do(currChannel,...args);
-    }else {
-      alert("too many args");
-    }
-  }
-}
-class Module
-{
-  constructor(name, needingEnergy, works, weight,html)
-  {
-    this.name = name;
-    this.needingEnergy = needingEnergy;
-    this.works = works;
-    this.weight = weight;
-    this.html=html;
-  }
-  do(idx, ...args)
-  {
-    this.works[idx](...args);
-  }
-}
 var baseFrame=new Frame('base',baseFrameHtml)
