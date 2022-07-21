79c955aa12110feea25a1d9bd5c9ffff0b38bc48
hierr777 <hi.illia.lavryniuk@gmail.com>
2022-07-21 14:15:08 +0300

    V1.045.3

--- js/rallybot.js
+++ js/rallybot.js
@@ -1,3 +1,497 @@
+var baseFrameHtml=`
+<div id="tridiv">
+  <div class="scene">
+    <div class="shape cuboid-1 body1">
+      <div class="face ft"></div>
+      <div class="face bk"></div>
+      <div class="face rt"></div>
+      <div class="face lt"></div>
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="cr cr-0">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-1">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-2">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-3">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+    </div>
+    <div class="shape cylinder-1 armLeft2">
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="face side s0"></div>
+      <div class="face side s1"></div>
+      <div class="face side s2"></div>
+      <div class="face side s3"></div>
+      <div class="face side s4"></div>
+      <div class="face side s5"></div>
+      <div class="face side s6"></div>
+      <div class="face side s7"></div>
+      <div class="face side s8"></div>
+      <div class="face side s9"></div>
+      <div class="face side s10"></div>
+      <div class="face side s11"></div>
+      <div class="face side s12"></div>
+      <div class="face side s13"></div>
+    </div>
+    <div class="shape cylinder-2 armRight2">
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="face side s0"></div>
+      <div class="face side s1"></div>
+      <div class="face side s2"></div>
+      <div class="face side s3"></div>
+      <div class="face side s4"></div>
+      <div class="face side s5"></div>
+      <div class="face side s6"></div>
+      <div class="face side s7"></div>
+      <div class="face side s8"></div>
+      <div class="face side s9"></div>
+      <div class="face side s10"></div>
+      <div class="face side s11"></div>
+      <div class="face side s12"></div>
+      <div class="face side s13"></div>
+    </div>
+    <div class="shape cylinder-3 armLeft1">
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="face side s0"></div>
+      <div class="face side s1"></div>
+      <div class="face side s2"></div>
+      <div class="face side s3"></div>
+      <div class="face side s4"></div>
+      <div class="face side s5"></div>
+      <div class="face side s6"></div>
+      <div class="face side s7"></div>
+      <div class="face side s8"></div>
+      <div class="face side s9"></div>
+      <div class="face side s10"></div>
+      <div class="face side s11"></div>
+      <div class="face side s12"></div>
+      <div class="face side s13"></div>
+    </div>
+    <div class="shape cylinder-4 armRight1">
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="face side s0"></div>
+      <div class="face side s1"></div>
+      <div class="face side s2"></div>
+      <div class="face side s3"></div>
+      <div class="face side s4"></div>
+      <div class="face side s5"></div>
+      <div class="face side s6"></div>
+      <div class="face side s7"></div>
+      <div class="face side s8"></div>
+      <div class="face side s9"></div>
+      <div class="face side s10"></div>
+      <div class="face side s11"></div>
+      <div class="face side s12"></div>
+      <div class="face side s13"></div>
+    </div>
+    <div class="shape cylinder-5 armLeft3">
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="face side s0"></div>
+      <div class="face side s1"></div>
+      <div class="face side s2"></div>
+      <div class="face side s3"></div>
+      <div class="face side s4"></div>
+      <div class="face side s5"></div>
+      <div class="face side s6"></div>
+      <div class="face side s7"></div>
+      <div class="face side s8"></div>
+      <div class="face side s9"></div>
+      <div class="face side s10"></div>
+      <div class="face side s11"></div>
+      <div class="face side s12"></div>
+      <div class="face side s13"></div>
+    </div>
+    <div class="shape cylinder-6 armRight3">
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="face side s0"></div>
+      <div class="face side s1"></div>
+      <div class="face side s2"></div>
+      <div class="face side s3"></div>
+      <div class="face side s4"></div>
+      <div class="face side s5"></div>
+      <div class="face side s6"></div>
+      <div class="face side s7"></div>
+      <div class="face side s8"></div>
+      <div class="face side s9"></div>
+      <div class="face side s10"></div>
+      <div class="face side s11"></div>
+      <div class="face side s12"></div>
+      <div class="face side s13"></div>
+    </div>
+    <div class="shape cuboid-2 body3">
+      <div class="face ft"></div>
+      <div class="face bk"></div>
+      <div class="face rt"></div>
+      <div class="face lt"></div>
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="cr cr-0">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-1">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-2">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-3">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+    </div>
+    <div class="shape cylinder-7 body2">
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="face side s0"></div>
+      <div class="face side s1"></div>
+      <div class="face side s2"></div>
+      <div class="face side s3"></div>
+      <div class="face side s4"></div>
+      <div class="face side s5"></div>
+      <div class="face side s6"></div>
+      <div class="face side s7"></div>
+      <div class="face side s8"></div>
+      <div class="face side s9"></div>
+      <div class="face side s10"></div>
+      <div class="face side s11"></div>
+      <div class="face side s12"></div>
+      <div class="face side s13"></div>
+    </div>
+    <div class="shape cylinder-8 leftLeg2">
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="face side s0"></div>
+      <div class="face side s1"></div>
+      <div class="face side s2"></div>
+      <div class="face side s3"></div>
+      <div class="face side s4"></div>
+      <div class="face side s5"></div>
+      <div class="face side s6"></div>
+      <div class="face side s7"></div>
+      <div class="face side s8"></div>
+      <div class="face side s9"></div>
+      <div class="face side s10"></div>
+      <div class="face side s11"></div>
+      <div class="face side s12"></div>
+      <div class="face side s13"></div>
+    </div>
+    <div class="shape cylinder-9 rightLeg2">
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="face side s0"></div>
+      <div class="face side s1"></div>
+      <div class="face side s2"></div>
+      <div class="face side s3"></div>
+      <div class="face side s4"></div>
+      <div class="face side s5"></div>
+      <div class="face side s6"></div>
+      <div class="face side s7"></div>
+      <div class="face side s8"></div>
+      <div class="face side s9"></div>
+      <div class="face side s10"></div>
+      <div class="face side s11"></div>
+      <div class="face side s12"></div>
+      <div class="face side s13"></div>
+    </div>
+    <div class="shape cylinder-10 leftLeg1">
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="face side s0"></div>
+      <div class="face side s1"></div>
+      <div class="face side s2"></div>
+      <div class="face side s3"></div>
+      <div class="face side s4"></div>
+      <div class="face side s5"></div>
+      <div class="face side s6"></div>
+      <div class="face side s7"></div>
+      <div class="face side s8"></div>
+      <div class="face side s9"></div>
+      <div class="face side s10"></div>
+      <div class="face side s11"></div>
+      <div class="face side s12"></div>
+      <div class="face side s13"></div>
+    </div>
+    <div class="shape cylinder-11 rightLeg1">
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="face side s0"></div>
+      <div class="face side s1"></div>
+      <div class="face side s2"></div>
+      <div class="face side s3"></div>
+      <div class="face side s4"></div>
+      <div class="face side s5"></div>
+      <div class="face side s6"></div>
+      <div class="face side s7"></div>
+      <div class="face side s8"></div>
+      <div class="face side s9"></div>
+      <div class="face side s10"></div>
+      <div class="face side s11"></div>
+      <div class="face side s12"></div>
+      <div class="face side s13"></div>
+    </div>
+    <div class="shape cylinder-12 neck">
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="face side s0"></div>
+      <div class="face side s1"></div>
+      <div class="face side s2"></div>
+      <div class="face side s3"></div>
+      <div class="face side s4"></div>
+      <div class="face side s5"></div>
+      <div class="face side s6"></div>
+      <div class="face side s7"></div>
+      <div class="face side s8"></div>
+      <div class="face side s9"></div>
+      <div class="face side s10"></div>
+      <div class="face side s11"></div>
+      <div class="face side s12"></div>
+      <div class="face side s13"></div>
+    </div>
+    <div class="shape cuboid-3 head2">
+      <div class="face ft"></div>
+      <div class="face bk"></div>
+      <div class="face rt"></div>
+      <div class="face lt"></div>
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="cr cr-0">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-1">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-2">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-3">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+    </div>
+    <div class="shape cuboid-4 head1">
+      <div class="face ft"></div>
+      <div class="face bk"></div>
+      <div class="face rt"></div>
+      <div class="face lt"></div>
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="cr cr-0">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-1">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-2">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-3">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+    </div>
+    <div class="shape cylinder-13 rightLeg3">
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="face side s0"></div>
+      <div class="face side s1"></div>
+      <div class="face side s2"></div>
+      <div class="face side s3"></div>
+      <div class="face side s4"></div>
+      <div class="face side s5"></div>
+      <div class="face side s6"></div>
+      <div class="face side s7"></div>
+      <div class="face side s8"></div>
+      <div class="face side s9"></div>
+      <div class="face side s10"></div>
+      <div class="face side s11"></div>
+      <div class="face side s12"></div>
+      <div class="face side s13"></div>
+    </div>
+    <div class="shape cylinder-14 leftLeg3">
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="face side s0"></div>
+      <div class="face side s1"></div>
+      <div class="face side s2"></div>
+      <div class="face side s3"></div>
+      <div class="face side s4"></div>
+      <div class="face side s5"></div>
+      <div class="face side s6"></div>
+      <div class="face side s7"></div>
+      <div class="face side s8"></div>
+      <div class="face side s9"></div>
+      <div class="face side s10"></div>
+      <div class="face side s11"></div>
+      <div class="face side s12"></div>
+      <div class="face side s13"></div>
+    </div>
+    <div class="shape cuboid-5 leftLeg4">
+      <div class="face ft"></div>
+      <div class="face bk"></div>
+      <div class="face rt"></div>
+      <div class="face lt"></div>
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="cr cr-0">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-1">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-2">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-3">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+    </div>
+    <div class="shape cuboid-6 rightLeg4">
+      <div class="face ft"></div>
+      <div class="face bk"></div>
+      <div class="face rt"></div>
+      <div class="face lt"></div>
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="cr cr-0">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-1">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-2">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+      <div class="cr cr-3">
+        <div class="face side s0"></div>
+        <div class="face side s1"></div>
+        <div class="face side s2"></div>
+      </div>
+    </div>
+    <div class="shape cylinder-15 armLeft4">
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="face side s0"></div>
+      <div class="face side s1"></div>
+      <div class="face side s2"></div>
+      <div class="face side s3"></div>
+      <div class="face side s4"></div>
+      <div class="face side s5"></div>
+      <div class="face side s6"></div>
+      <div class="face side s7"></div>
+      <div class="face side s8"></div>
+      <div class="face side s9"></div>
+      <div class="face side s10"></div>
+      <div class="face side s11"></div>
+      <div class="face side s12"></div>
+      <div class="face side s13"></div>
+    </div>
+    <div class="shape cylinder-16 armRight4">
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+      <div class="face side s0"></div>
+      <div class="face side s1"></div>
+      <div class="face side s2"></div>
+      <div class="face side s3"></div>
+      <div class="face side s4"></div>
+      <div class="face side s5"></div>
+      <div class="face side s6"></div>
+      <div class="face side s7"></div>
+      <div class="face side s8"></div>
+      <div class="face side s9"></div>
+      <div class="face side s10"></div>
+      <div class="face side s11"></div>
+      <div class="face side s12"></div>
+      <div class="face side s13"></div>
+    </div>
+    <div class="shape cuboid-7 armLeftSlot1">
+      <div class="face ft"></div>
+      <div class="face bk"></div>
+      <div class="face rt"></div>
+      <div class="face lt"></div>
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+    </div>
+    <div class="shape cuboid-8 armLeftSlot2">
+      <div class="face ft"></div>
+      <div class="face bk"></div>
+      <div class="face rt"></div>
+      <div class="face lt"></div>
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+    </div>
+    <div class="shape cuboid-9 armRightSlot2">
+      <div class="face ft"></div>
+      <div class="face bk"></div>
+      <div class="face rt"></div>
+      <div class="face lt"></div>
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+    </div>
+    <div class="shape cuboid-10 armRightSlot1">
+      <div class="face ft"></div>
+      <div class="face bk"></div>
+      <div class="face rt"></div>
+      <div class="face lt"></div>
+      <div class="face bm"></div>
+      <div class="face tp"></div>
+    </div>
+  </div>
+</div>
+`;
 //The RallyBot Class
 class RallyBot {
   #maxCurrentSpeed=0;
